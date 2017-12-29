d9737f2c6c594e9fd4a0709192a1dc9f64c50acf
Bekah Hawrot Weigel <rhawrot@gmail.com>
2017-12-29 22:52:12 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,33 @@
+function theBeatlesPlay(musicians, instruments){
+  for (var i = []; i<musicians.length; i++) {
+    if (i === 0){
+ i.push( "John Lennon plays guitar" );}
+ else{i.push(`$(musicans) plays $(instruments)`)}
+}
+  return i
+}
+
+const facts = [
+ "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+
+function johnLennonFacts(array){
+  var newFacts = []
+  var i = 0
+  while (i<facts.length) 
+    newFacts.push(facts[i] + "!!!")
+  
+  return newFacts 
+}
+
+function iLoveTheBeatles(n) {
+  var i = []
+  do{i.push("I love the Beatles!");
+  n++;
+  }
+  while (i<15);
+  return array;
+}
